8ca4b46d60109f7a49cab86ba758508dcfcdf626
Carlos Vico Villalba <carlos.vico.villalba@cern.ch>
2023-10-17 17:11:49 +0200

    General script designed to make results always reproducible

--- TTHAnalysis/python/plotter/wz-run3/scripts/make_plots.sh
+++ TTHAnalysis/python/plotter/wz-run3/scripts/make_plots.sh
@@ -10,7 +10,7 @@ extra="${@:2}"
 function append_to_path (){
     # This function appends an additional name to the output
     # name given to the script
-    new_extra=$(echo "$extra" | sed -E "s/(--outname [^[:space:]]+)/\1\/$1/")
+    outname=$(echo "$extra" | sed -E "s/(--outname [^[:space:]]+)/\1\/$1/")
 }
 
 # Since these scripts are for specific studies, let's just add a basic outname that
@@ -21,38 +21,52 @@ if [[ -z $isThereOutname ]]; then
 fi
 
 
-# Produce plots per flavor
-if [[ $mode == *perflav* ]]; then
+# --------------------------------------------- #
+#   1. PRODUCE ALL PLOTS USED IN THE ANALYSIS   #
+# --------------------------------------------- #
+if [[ $mode == *analysis* ]]; then
+
+    echo -e $GREEN SUBMITTING for flavor: inclusive $NC
+    append_to_path inclusive
+    python wz-run3/wz-run.py plot $outname --plot-group main
+    echo "-----------------------------"
+
+    # Produce plots per flavor
+    if [[ $mode == *perflav* ]]; then
     for flav in eee eem mme mmm; do
         # Create an specific folder for each flavor channel
         append_to_path $flav
-
         echo -e $GREEN SUBMITTING for flavor: $flav $NC
-        python wz-run3/wz-run.py plot --extra "-E ^flav_${flav}" $new_extra --plot-group main
+        python wz-run3/wz-run.py plot --extra "-E ^flav_${flav}" $outname --plot-group main
         echo "-----------------------------"
     done
+    fi
 fi
 
-# Produce plots for trigger efficiency measuremtn
+# ------------------------------------------------------ #
+#   2. PRODUCE PLOTS FOR TRIGGER EFFICIENCY MEASUREMENT  #
+# ------------------------------------------------------ #
 if [[ $mode == *trigger_eff* ]]; then
     cuts="wz-run3/common/cuts_trigger.txt"
     mca="wz-run3/mca/mca_wz_triggerEff.txt"
-
     
 
-    # Numerator
-    echo -e $GREEN ">> Command for running the plot for the numerator $NC"
-    append_to_path numerator
-    python wz-run3/wz-run.py plot --mca $mca --cutfile $cuts --extra "-E ^trigger" $new_extra --plot-group trigger
-    echo "-----------------------------"
+    for barrelN in Inclusive barrel0 barrel1 barrel2 barrel3; do 
+        
+        if [[ ! $barrelN == Inclusive ]]; then extracut=" -E ^$barrelN"; fi
+        # Numerator
+        echo -e $GREEN ">> Command for running the plot for the numerator: $barrelN $NC"
+        append_to_path "numerator\/$barrelN"
+        python wz-run3/wz-run.py plot --mca $mca --cutfile $cuts --extra "--uf -E ^trigger $extracut" $outname --plot-group trigger
+        echo "-----------------------------"
 
-    
-    # Denominator
-    echo -e $GREEN ">> Command for running the plot for the denominator $NC" 
-    append_to_path denominator
-    python wz-run3/wz-run.py plot --mca $mca --cutfile $cuts  $new_extra --plot-group trigger
-    echo "-----------------------------"
+        
+        # Denominator
+        echo -e $GREEN ">> Command for running the plot for the denominator: $barrelN $NC"
+        append_to_path "denominator\/$barrelN"
+        python wz-run3/wz-run.py plot --mca $mca --cutfile $cuts  $outname --plot-group trigger --extra " --uf $extracut "
+        echo "-----------------------------"
+    done
 
 fi
-
 
