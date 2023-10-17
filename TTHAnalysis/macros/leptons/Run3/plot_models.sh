149d819a5212b7e830530df49b54aece34980fc6
Carlos Vico Villalba <carlos.vico.villalba@cern.ch>
2023-10-17 17:15:45 +0200

    Updated scripts for training mva

--- TTHAnalysis/macros/leptons/Run3/plot_models.sh
+++ TTHAnalysis/macros/leptons/Run3/plot_models.sh
@@ -1,11 +1,7 @@
 # --- Muon and electrons with ttH preselection
-./plot_model.sh mu_tth_v2_nanovars mu_tth performance_tth_muons
-./plot_model.sh el_tth_v2_nanovars el_tth performance_tth_electrons
+./plot_model.sh mu_tth_trainSept2023 mu_tth performance_tth_muons
+./plot_model.sh el_tth_trainSept2023 el_tth performance_tth_electrons
 
 # --- Muon and electrons with ttW preselection
-./plot_model.sh mu_ttw_v2_nanovars mu_ttw performance_ttw_muons
-./plot_model.sh el_ttw_v2_nanovars el_ttw performance_ttw_electrons
-
-# --- Muon and electrons with ttW preselection
-./plot_model.sh mu_wz_v2_nanovars mu_wz performance_wz_muons
-./plot_model.sh el_wz_v2_nanovars el_wz performance_wz_electrons
+./plot_model.sh mu_wz_trainSept2023 mu_wz performance_wz_muons
+./plot_model.sh el_wz_trainSept2023 el_wz performance_wz_electrons
