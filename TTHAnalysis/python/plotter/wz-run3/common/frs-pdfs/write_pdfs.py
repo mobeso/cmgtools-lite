576baab6c5204aa00ded53b6ca28d44922795e8b
Carlos Vico Villalba <carlos.vico.villalba@cern.ch>
2024-02-15 14:45:28 +0100

    Updated uncertainties

--- TTHAnalysis/python/plotter/wz-run3/common/frs-pdfs/write_pdfs.py
+++ TTHAnalysis/python/plotter/wz-run3/common/frs-pdfs/write_pdfs.py
@@ -3,9 +3,7 @@ import os
 pdfs_indices = range(104)
 template = "LHEPdfWeight[{idx}]"
 
-txt = """weight : Alt$(LHEPdfWeight[{idx}], 1)
-alt-norm : Alt$(LHEPdfSumw[{idx}]/LHEPdfSumw[0], 1)
-"""
+txt = """weight : Alt$(LHEPdfWeight[{idx}], 1)"""
 filename_template = "fr-pdf_{idx}.txt"
 for pdfindex in pdfs_indices:
     f = open(filename_template.format(idx = pdfindex), "w")
